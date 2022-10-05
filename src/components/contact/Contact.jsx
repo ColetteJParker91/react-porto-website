c7674076d37441a33790f834c7493cf7ce15695a
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-10-06 01:21:20 +0200

    Added emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,33 @@
-import React from "react";
 import "./contact.css";
 import { HiOutlineMail } from "react-icons/hi";
 import { GrLinkedin } from "react-icons/gr";
 import { SiWhatsapp } from "react-icons/si";
 
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_d3tdchi",
+        "template_u57eqee",
+        form.current,
+        "DcOJGRffxyfClIOtd"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section id="contact">
       <h5>Get in touch</h5>
@@ -12,7 +35,7 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <HiOutlineMail />
+            <HiOutlineMail className="contact_option-icon" />
             <h4>Email</h4>
             <h5>colette.joy.parker@gmail.com</h5>
             <a href="mailto:colette.joy.parker@gmail.com" target="_blank">
@@ -20,7 +43,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact_option">
-            <GrLinkedin />
+            <GrLinkedin className="contact_option-icon" />
             <h4>Linkedin</h4>
             <h5>Colette Parker</h5>
             <a
@@ -31,11 +54,11 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact_option">
-            <SiWhatsapp />
+            <SiWhatsapp className="contact_option-icon" />
             <h4>Whatsapp</h4>
             <h5>Message and Calls</h5>
             <a
-              href="https://api.whatsapp.com/send?phone+491794274187"
+              href="https://api.whatsapp.com/send?phone=+491794274187"
               target="_blank"
             >
               Send me a whatsapp
@@ -43,7 +66,22 @@ const Contact = () => {
           </article>
         </div>
         {/*END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button className="btn btn-primary">Send message</button>
+        </form>
       </div>
     </section>
   );
